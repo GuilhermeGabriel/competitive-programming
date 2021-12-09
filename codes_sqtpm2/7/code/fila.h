c24f6f9dd6d602fa878cf0e80652a52f58b1ae2d
Guilherme <guilhermegabriel@gmail.com>
2021-12-09 08:56:22 -0300

    remove some files

--- codes_sqtpm2/7/code/fila.h
+++
@@ -1,8 +0,0 @@
-#include "lista.h"
-typedef Lista Fila;
-
-void cria_fila(Fila **f);
-void insere_fila(Fila *f, int v, int i, int j);
-void remove_fila(Fila *f);
-void libera_fila(Fila **f);
-void mostra_fila(Fila **f);
